2a471919d8b071b598fe72e0b02a70850dcdff72
QuentinBELAGHROUZ <belaghrouz.quentin@gmail.com>
2018-11-10 18:25:13 +0100

    Fin exo analyse + Js + Debut TP Intégration

--- JS/Exos Chap3/script.js
+++ JS/Exos Chap3/script.js
@@ -2,6 +2,7 @@ let r = ['01', '02', '03', '04', '05'];
 let d = ['ecran', 'clavier', 'souris', 'imprimante', 'enceinte'];
 let p = [150, 20, 10, 100, 50];
 
+
 let tab_ex8 = ['r01*ecran*150', 'r02*clavier*20', 'r03*souris*10'];
 
 function printLength(text) {
@@ -67,6 +68,64 @@ function exo8(){
     document.getElementById('prix').value = tmp[2];
 }
 
-function exo9(){
+function exo10(){
+    let nb_aleatoire = 42;
+    let nb_essais_restants = 3;
+    let saisie;
+    let gagne = false;
 
+    do{
+       saisie =  prompt('Essayez de deviner le nombre mystere ( ' + nb_essais_restants + ' essais) ');
+        if(saisie > nb_aleatoire)
+            alert('Trop grand');
+        else if(saisie < nb_aleatoire)
+            alert('Trop petit');
+
+        nb_essais_restants -= 1;
+
+        if(nb_essais_restants == 0)
+            break;
+    }while(saisie != nb_aleatoire );
+
+    if(nb_essais_restants == 0)
+        alert('Perdu !')
+    else
+        alert('Gagné !');
+
+}
+
+function exo11(){
+    let txm = prompt('Taux Mensuel');
+    let mt = prompt('Montant du credit');
+    let duree = prompt('Duree');
+
+    let ch =  '<table>'+
+        '<tr>'+
+        '<th>Periode</th>'+
+        '<th>Capital Restant</th>'+
+        '<th>Interet</th>'+
+        '<th>Amortissement</th>'+
+        '<th>Mensualite</th>'+
+        '</tr>';
+
+       ch += '</table>';
+
+    document.open();
+    document.write(ch);
+    document.close();
+}
+
+function coordonnees(v){
+    v= v.split(':');
+    let tmp =[];
+    let n1 = v[0];
+    let n2 = v[1] + '.' + v[2]
+    tmp.push(n1);
+    tmp.push(n2);
+    return tmp;
+}
+
+function exo12(v){
+    console.log(coordonnees(v)[0], coordonnees(v)[1]);
+    return L.marker(coordonnees(this.value)[0], coordonnees(this.value)[1]);
 }
