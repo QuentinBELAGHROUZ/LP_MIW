6128655eb89b9f3ea36a3d99e8503dd7f8de389c
QuentinBELAGHROUZ <belaghrouz.quentin@gmail.com>
2018-11-26 22:08:27 +0100

    debut exo ajax

---
+++ JS/AJAX/exFinal/response.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type:text/xml');
+
+try{
+    $bdd = new PDO(
+        'mysql:host=localhost;dbname=miw_world;charset=utf8',
+        'root',
+        'root',
+        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING)
+
+    );
+}catch(Exception $e){
+    die('Erreur : '.$e->getMessage());
+}
+
+$result = $bdd -> query('SELECT * FROM categories');
+
+
+
+
